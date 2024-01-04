9845be1db515ed11d2204ab9f0cba5218781ff27
Mgkusumaputra <muhammadgaruda55@yahoo.com>
2024-01-04 22:56:38 +0700

    feat: store logic

--- src/stores.ts
+++ src/stores.ts
@@ -1,11 +1,26 @@
-import { writable } from "svelte/store";
+import { localStore } from "./localStore";
 
-// const isoDate = new Date();
+const initialDatas = [
+  {
+    id: 1,
+    name: "Budi",
+    email: "Budi@mail.co",
+    dateOfBirth: "2000-12-09",
+    age: 24,
+    gender: "Male",
+    usedSvelte: true,
+    description: "Im Budi",
+  },
+  {
+    id: 2,
+    name: "Dudi",
+    email: "Dudi@mail.co",
+    dateOfBirth: "2000-12-09",
+    age: 24,
+    gender: "Male",
+    usedSvelte: false,
+    description: "Im Dudi",
+  },
+];
 
-export const name = writable("");
-export const email = writable("");
-export const date = writable(new Date());
-export const age = writable(18);
-export const gender = writable("");
-export const isEverUsedSvelte = writable(false);
-export const description = writable("");
+export const datas = localStore("form-data", initialDatas);
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@
 export default {
   content: [
     "./src/**/*.{html,js,svelte,ts}",
+    "./src/components/**/*.{html,js,svelte,ts}",
     "./node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}",
   ],
   theme: {
